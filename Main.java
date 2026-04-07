12198773d28b9560fba06351c7f310c739f49064
Oscar <ocastillob3@miumg.edu.gt>
2026-04-06 22:15:45 -0600

    6 Termometro Class

--- Main.java
+++ Main.java
@@ -196,3 +196,21 @@ class Estudiante {
      return promedio >= 85;
  }
 }
+
+//6) Termometro
+class Termometro {
+ private double celsius;
+
+ public void setCelsius(double temperatura) {
+     if (temperatura >= -273.15) {
+         this.celsius = temperatura;
+     } else {
+         System.out.println("Temperatura inválida");
+     }
+ }
+
+ public double getFahrenheit() {
+     return (celsius * 9 / 5) + 32;
+ }
+}
+

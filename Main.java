e115bdc9f4a1b42f90ba28b893fd346abb5d2de2
Oscar <ocastillob3@miumg.edu.gt>
2026-04-06 22:28:03 -0600

    7 Reloj Class

--- Main.java
+++ Main.java
@@ -214,3 +214,26 @@ class Termometro {
  }
 }
 
+//7) Reloj
+class Reloj {
+ private int hora, minuto, segundo;
+
+ public void setHora(int hora) {
+     if (hora >= 0 && hora <= 23) this.hora = hora;
+     else System.out.println("Hora inválida");
+ }
+
+ public void setMinuto(int minuto) {
+     if (minuto >= 0 && minuto <= 59) this.minuto = minuto;
+     else System.out.println("Minuto inválido");
+ }
+
+ public void setSegundo(int segundo) {
+     if (segundo >= 0 && segundo <= 59) this.segundo = segundo;
+     else System.out.println("Segundo inválido");
+ }
+
+ public String mostrarHora() {
+     return String.format("%02d:%02d:%02d", hora, minuto, segundo);
+ }
+}

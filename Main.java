850d85efd6b3f680b593cf7bda9e6074dda36ed6
Oscar <ocastillob3@miumg.edu.gt>
2026-04-06 20:20:13 -0600

    1 Persona Class

--- Main.java
+++ Main.java
@@ -77,3 +77,23 @@ public class Main {
         f.mostrarFactura();
     }
 }
+
+
+//1) Persona
+class Persona {
+ private String nombre;
+ private int edad;
+
+ public String getNombre() { return nombre; }
+ public void setNombre(String nombre) { this.nombre = nombre; }
+
+ public int getEdad() { return edad; }
+ public void setEdad(int edad) {
+     if (edad < 0) {
+         System.out.println("Edad inválida, se asigna 0");
+         this.edad = 0;
+     } else {
+         this.edad = edad;
+     }
+ }
+}

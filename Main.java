0cec3d4d4568b732e770f6323a443c5cf104de53
Oscar <ocastillob3@miumg.edu.gt>
2026-04-06 22:45:34 -0600

    8 Contacto Class

--- Main.java
+++ Main.java
@@ -237,3 +237,24 @@ class Reloj {
      return String.format("%02d:%02d:%02d", hora, minuto, segundo);
  }
 }
+
+//8) Contacto
+class Contacto {
+ private String nombre, telefono, email;
+
+ public void setNombre(String nombre) { this.nombre = nombre; }
+
+ public void setTelefono(String telefono) {
+     if (telefono.length() == 8) this.telefono = telefono;
+     else System.out.println("Teléfono inválido");
+ }
+
+ public void setEmail(String email) {
+     if (email.contains("@")) this.email = email;
+     else System.out.println("Email inválido");
+ }
+
+ public void mostrarContacto() {
+     System.out.println(nombre + " - " + telefono + " - " + email);
+ }
+}
